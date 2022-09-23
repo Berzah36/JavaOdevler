6b1c343339638ff86cf9d0daf73c0eeda401bee9
Bora Selimoğlu <bora.selimoglu@turktelekom.com.tr>
2022-09-23 10:32:11 +0300

    odevler1

---
+++ Variables/Veriables/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ogrenciSayi = 18;
+        String mesaj = "ÖĞRENCİ SAYISI :";
+        System.out.println(mesaj + ogrenciSayi );
+        System.out.println(mesaj + ogrenciSayi );
+        System.out.println(mesaj + ogrenciSayi );
+
+
+
+
+
+
+
+
+    }
+}
---
+++ switchDemo/switchDemo/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        char grade = 'F';
+
+        switch (grade) {
+            case 'A':
+                System.out.println("Çok başarılı = Geçtiniz");
+                break;
+            case 'B':
+                System.out.println("Başarılı = Geçtiniz");
+                break;
+            case 'C':
+                System.out.println("Orta = Geçtiniz");
+                break;
+            case 'D':
+                System.out.println("Fena değil = Geçtiniz");
+                break;
+            case 'F':
+                System.out.println("Kötü = Kaldınız");
+                break;
+            default:
+                System.out.println("Geçersiz not girdiniz");
+
+
+        }
+
+    }
+}
